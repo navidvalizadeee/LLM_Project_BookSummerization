e68fe458ccc2053fa8a6ac718db56da2212a1bb8
farhad-b <f.baghyari@yahoo.com>
2023-11-06 15:37:05 -0500

    using chat completion

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def extract_entities(text):
     try:
         # Call the OpenAI API to extract entities from the text
-        response = openai.Completion.create(
-          engine="text-davinci-003",  # or the latest engine available
-          prompt=f"Identify and list all the unique entities from the following text:\n\n{text}",
-          max_tokens=150
+        # response = openai.Completion.create(
+        #   engine="gpt-3.5-turbo",  # or the latest engine available
+        #   prompt=f"Identify and list all the unique entities from the following text:\n\n{text}",
+        #   max_tokens=150
+        # )
+
+        response = openai.ChatCompletion.create(
+          model='gpt-3.5-turbo',
+          messages=[{'role': 'system', 'content': f"Identify and list all the unique entities from the following, make sure you don't miss anything."},
+                     {'role': 'user', 'content': f"{text}"}]
         )
-
         # Extract the entities from the response
-        entities = response.choices[0].text.strip().split('\n')
+        choices = response['choices']
+        firstMessage = choices[0].message.content
+        role = choices[0].message.role
+        print(role, "\n", firstMessage)
 
-        return entities
     except Exception as e:
         print(f"An error occurred: {e}")
         return []
@@ -25,6 +32,3 @@ text_to_analyze = "Alice is a data scientist at Initech. She works with Bob, a s
 
 # Extract entities
 entities = extract_entities(text_to_analyze)
-
-# Now entities contains a list of entities extracted from the text
-print("Extracted Entities:", entities)
