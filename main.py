b55bc298327436a0d5b44aeb8e62356ac4a79da9
farhad.baghyari <farhad.baghyari@forvest.io>
2023-11-06 19:02:43 -0500

    lots of improvement

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import openai
 import os
+import json
+import pprint
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-# mainTextPath = "Data/As I Lay Dying.txt"
-mainTextPath = "Data\As I Lay Dying.txt"
+mainTextPath = "Data/As I Lay Dying.txt"
+# mainTextPath = "Data\As I Lay Dying.txt"
 # mainText = open(mainTextPath, "r").read()
 
 messageHistory = []
 systemContent = """
 Identify and list all the unique entities from the given text, make sure you don't miss anything.
-Important entities are charachters, relations, events, locations, objects, and concepts.
+From now on we call these entities as Nodes.
+Nodes are charachters, relations, events, locations, objects, and concepts.
+Node types must be mentioned in the output.
+Do not include any Nodes that are not in the text.
+Do not use prenouns or pronouns, use the full name of the Nodes. 
+This is a Node sample: {"id": "Ada", "type": "charachter"}
 Also a string in json format is always provided. if it is empty it mean no data is extracted yet.
+You must also return the Nodes in JSON format.
+Moreover, show connections between the Nodes as Edges by using the following format:
+{"source": "Node1_id", "target": "Node2_id", "relation": "Relation1", "senti": "positive"}
+Relations are verbs, adjectives, and adverbs, none of them are unique names, all of them are general terms.
 """
+
 systemMessage = {"role": 'system', "content":systemContent}
+
 messageHistory.append(systemMessage)
-def extract_entities(text):
+def extract_entities(messages):
     try:
         response = openai.ChatCompletion.create(
-          model='gpt-3.5-turbo',
-          messages=[{'role': 'system', 'content': f""},
-                     {'role': 'user', 'content': f"{text}"}]
+        #   model='gpt-3.5-turbo',
+          model='gpt-3.5-turbo-1106',
+          messages=messages,
+          temperature=0,
+        #   max_tokens=1000,
+          response_format= {'type': 'json_object'}
         )
         # Extract the entities from the response
         choices = response['choices']
         firstMessage = choices[0].message.content
         role = choices[0].message.role
-        print(role, "\n", firstMessage)
+        # print(role, "\n", firstMessage)
+        return response
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -48,14 +65,22 @@ with open(mainTextPath, 'r') as file:
 paraCount = len(paragraphs)
 paraphsPerMessage = 50
 historyParaphsPerMessage = round(paraphsPerMessage * 0.1)
-maxParaphParsed = 51
+maxParaphParsed = 150
 
 for i in range(0, paraCount, paraphsPerMessage):
     if i >= maxParaphParsed:
         break
     content = ""
-    for j in range(i):
+    for j in range(i, i + paraphsPerMessage):
         content += paragraphs[j] + "\n"
     newMessage = {"role": 'user', "content": content}
-    messageHistory.append(newMessage)
-    extract_entities(content)
+    messages = messageHistory + [newMessage]
+    print("")
+    print("sending request to openai")
+    response = extract_entities(messages)
+    try:
+        jsonRes = json.loads(response.choices[0].message.content)
+        pprint.pprint(jsonRes)
+    except Exception as e:
+        print(response.choices[0].message.content)
+        print("could not parse json")
